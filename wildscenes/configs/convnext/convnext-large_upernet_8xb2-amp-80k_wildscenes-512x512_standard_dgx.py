64ca0d1fe645afa824f479f861a795308ca8b917
Stephen Hausler <stephen.hausler@data61.csiro.au>
2024-04-09 16:46:17 +1000

    Adding more configs for different 2d models

---
+++ wildscenes/configs/convnext/convnext-large_upernet_8xb2-amp-80k_wildscenes-512x512_standard_dgx.py
@@ -0,0 +1,16 @@
+_base_ = [
+        "convnext-large_upernet_8xb2-amp-80k_wildscenes-512x512_template.py", "../_base_/datasets/wildscenes_standard_dgx.py"
+        ]
+
+num_classes = _base_.num_classes
+crop_size = _base_.crop_size
+data_preprocessor = dict(size=crop_size)
+decode_head = dict(num_classes=num_classes)
+aux_head = dict(num_classes=num_classes)
+test_cfg = dict(crop_size=crop_size)
+model = dict(
+    data_preprocessor=data_preprocessor,
+    decode_head=decode_head,
+    auxiliary_head=aux_head,
+    test_cfg=test_cfg
+)
