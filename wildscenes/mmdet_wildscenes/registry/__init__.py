02de790a347f4f831423afd7cdbc77fcc820d95f
Stephen Hausler <hau047@dgxh4-2.hpc.csiro.au>
2024-04-11 14:51:53 +1000

    adding more 3d files

---
+++ wildscenes/mmdet_wildscenes/registry/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+# No modifications have been made to this file except for this notice.
+
+from .registry3d import (RUNNERS3D, DATASETS)
+
+__all__ = [
+    'RUNNERS3D', 'DATASETS'
+]
